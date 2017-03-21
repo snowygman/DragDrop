2bd322642e9af68fe8f1ad687e71c4351fcd7f99
noelg <goldenchild_gt@yahoo.com>
2017-03-21 08:28:51 -0400

    drop

---
+++ DragDrop/Scripts/MicroScramble.js
@@ -0,0 +1,29 @@
+﻿/// <reference path="jquery-3.1.1.js" />
+
+var $draggedItem;
+$(document).ready(function () {
+    $('.item').on('dragstart', dragging);
+    $('.item').on('dragend', draggingEnded);
+    $(".hole").on('dragenter', preventDefault);
+    $(".hole").on('dragover', preventDefault);
+    $(".hole").on('drop', dropItem);
+
+});
+function dragging(e) {
+    $(e.target).addClass('dragging');
+    $draggedItem = $(e.target);
+}
+function draggingEnded(e) {
+    $(e.target).removeClass('dragging');
+}
+
+function preventDefault(e) {
+    e.preventDefault();
+}
+function dropItem(e) {
+    var hole = $(e.target);
+    if (hole.hasClass('hole') && hole.children().length == 0) {
+        $draggedItem.detach();
+        $draggedItem.appendTo($(e.target));
+    }
+}
